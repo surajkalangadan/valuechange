7beddd29487b0bfa22b50cb18c2279f38662fd3f
surajkalangadan <surajkalangadan935@gmail.com>
2023-07-25 00:21:36 -0700

    providervalluechange

---
+++ android/app/src/main/kotlin/com/example/fffff/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fffff
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen1.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class A with ChangeNotifier{
+  int value=0;
+  void changeValue(){
+    value=value+1;
+    notifyListeners();
+  }
+}
